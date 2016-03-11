354c484bf9ffd90172cb26bf634deaeb72ca1bf4
JelX7ouss <jeliti7oussam@gmail.com>
2016-03-11 13:09:43 +0100

    improved comments

--- js/drockchat-conf.js
+++ js/drockchat-conf.js
@@ -10,11 +10,10 @@
 	* I wanted to make sure all values in drupalSettings Object are safe
 	* Will cast them later to JSON and get data from the drockchat-widget.js file
 	* However a Drupal way to do this is highly recommended
+	* 
+	* console.log(FORM_CONFIG_VARS["server_conf"]);
+	* console.log(FORM_CONFIG_VARS["port_conf"]);
 	*/
-
-	console.log(FORM_CONFIG_VARS["server_conf"]);
-	console.log(FORM_CONFIG_VARS["port_conf"]);
-	
 
 
 })(jQuery);
