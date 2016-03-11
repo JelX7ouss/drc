203bb1102750ac4cb16fa8ad1dae918629eab904
JelX7ouss <jeliti7oussam@gmail.com>
2016-03-11 11:22:13 +0100

    deleted default values in .settings.yml

--- js/drockchat-conf.js
+++ js/drockchat-conf.js
@@ -10,9 +10,9 @@
 	* I wanted to make sure all values in drupalSettings Object are safe
 	* Will cast them later to JSON and get data from the drockchat-widget.js file
 	* However a Drupal way to do this is highly recomended
-	* 
-	* console.log(FORM_CONFIG_VARS["server_conf"]);
-	* console.log(FORM_CONFIG_VARS["port_conf"]);
+	*
+	console.log(FORM_CONFIG_VARS["server_conf"]);
+	console.log(FORM_CONFIG_VARS["port_conf"]);
 	*/
 
 
