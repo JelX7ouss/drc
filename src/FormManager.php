9496bee444ce4210d68d890213c8710d36aacf8a
Jelliti Houssam <jeliti7oussam@gmail.com>
2016-03-15 15:46:20 +0100

    Update FormManager.php

--- src/FormManager.php
+++ src/FormManager.php
@@ -21,12 +21,12 @@ class FormManager {
 	
 	
 	// This method check if user have typed the server:port
-	// which have been pointed to on one the Config page
+	// which have been pointed to on the Config page
 	public static function serverRun($url, $port){
 
 		// user running on http, https, or local
 		// thought of using an array of custom ports but
-		// '===' and '||' operators are better to use
+		// I think '===' and '||' operators are better to use
 		if($port === 80 || $port === 443 || $port === 3000){
 
 			if(!empty($url)){
